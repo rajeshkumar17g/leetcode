44394b36f1d1ae43cf24d51570ca75095e2b8603
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-04-08 15:31:36 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (73.30%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root==None:
+            return 0
+        
+        res=[]
+        q=deque([root])
+
+        while q:
+            level=[]
+            for _ in range(len(q)):
+                node=q.popleft()
+                level.append(node.val)
+                if node.left!=None:
+                    q.append(node.left)
+                if node.right!=None:
+                    q.append(node.right)
+            res.append(level)
+        return len(res)
