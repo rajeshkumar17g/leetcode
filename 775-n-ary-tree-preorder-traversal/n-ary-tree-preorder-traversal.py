3a45e2e34d05a9eadb1aedf0b207f2f05549209f
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-04-08 09:22:33 +0530

    Time: 50 ms (82.29%) | Memory: 20.9 MB (69.09%) - LeetSync

--- 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.py
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.py
@@ -3,30 +3,19 @@
 class Node:
     def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
         self.val = val
-        self.children = children
+        self.children = children [2,3,4,5]
 """
+
 class Solution:
     def preorder(self, root: 'Node') -> List[int]:
-        
         def traversal(root):
-            if root:
-                res.append(root.val)
-                for child in root.children:
-                    traversal(child)
-
+            if root==None:
+                return
+            
+            res.append(root.val)
+            for child in root.children: #[10,20,30,40l50]
+                traversal(child)
         #---------------------------
         res=[]
         traversal(root)
         return res
-
-
-
-
-
-
-
-
-
-
-
-
