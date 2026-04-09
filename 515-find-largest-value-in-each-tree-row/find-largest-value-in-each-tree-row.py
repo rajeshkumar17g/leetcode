ca5c6626b8ce9508001e302f88fa021fc8e9afb5
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-04-09 09:21:38 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (62.75%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.py
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.py
@@ -8,11 +8,11 @@ class Solution:
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
         if root==None:
             return []
-        #FIFO
-        q=deque([root]) # q=[root]. # deque array. .append(). .popleft()
+        
         res=[]
+        q=deque([root])
 
-        while q: # while q is not empty 
+        while q:
             level=[]
             for _ in range(len(q)):
                 node=q.popleft()
@@ -22,5 +22,4 @@ class Solution:
                 if node.right!=None:
                     q.append(node.right)
             res.append(max(level))
-        
         return res
