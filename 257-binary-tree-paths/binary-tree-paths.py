c8d45650fba314f608292d6acecc9f411544be6b
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-04-11 16:50:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (70.21%) - LeetSync

---
+++ 257-binary-tree-paths/binary-tree-paths.py
@@ -0,0 +1,20 @@
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        
+        def helper(root,subset):
+            if root.left==None and root.right==None:
+                res.append("->".join(subset))
+                return
+            if root.left!=None:
+                subset.append(str(root.left.val))
+                helper(root.left,subset)
+                subset.pop()
+            
+            if root.right!=None:
+                subset.append(str(root.right.val))
+                helper(root.right,subset)
+                subset.pop()
+        #------------------------
+        res=[]
+        helper(root,[str(root.val)])
+        return res
