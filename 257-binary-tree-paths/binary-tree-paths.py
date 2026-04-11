54114b3b9bd7b9baf39a015a822dffdacf7ac141
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-04-11 16:57:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (91.20%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.py
+++ 257-binary-tree-paths/binary-tree-paths.py
@@ -1,6 +1,6 @@
 class Solution:
-    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        def helper(root, crr):
+    """
+    def helper(root, crr):
             if root==None: # crossed leaf node
                 return None
             if root.left==None and root.right==None:
@@ -12,4 +12,23 @@ class Solution:
         #-----------------------------------------------------------------
         res = []
         helper(root, "")
+        return res"""
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        
+        def helper(root,subset):
+            if root.left==None and root.right==None:
+                res.append("->".join(subset))
+                return
+            if root.left!=None:
+                subset.append(str(root.left.val))
+                helper(root.left,subset)
+                subset.pop()
+            
+            if root.right!=None:
+                subset.append(str(root.right.val))
+                helper(root.right,subset)
+                subset.pop()
+        #------------------------
+        res=[]
+        helper(root,[str(root.val)])
         return res
