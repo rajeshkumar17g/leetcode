0e8fabd1b07c5232191b4805f21b147239f09925
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-04-11 09:29:39 +0530

    Time: 3 ms (6.64%) | Memory: 19.2 MB (98.09%) - LeetSync

--- 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
@@ -4,8 +4,7 @@ class Solution:
             return True
         if p==None or q==None or p.val!=q.val:
             return False
-        return (self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right) or self.isSameTree(p.left,q.right) and self.isSameTree(p.right,q.left))
+        return (self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)) or (self.isSameTree(p.left,q.right) and self.isSameTree(p.right,q.left))
 
     def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
         return self.isSameTree(root1,root2)
-        
