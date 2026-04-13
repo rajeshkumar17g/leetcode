c172798b282c03bda77c564f5d5063f74e641ce5
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-04-13 15:23:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (69.08%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.py
+++ 257-binary-tree-paths/binary-tree-paths.py
@@ -1,34 +1,16 @@
 class Solution:
-    """
-    def helper(root, crr):
-            if root==None: # crossed leaf node
-                return None
-            if root.left==None and root.right==None:
-                res.append(crr + str(root.val))
-                return
-
-            helper(root.left, crr + str(root.val) + "->") 
-            helper(root.right, crr + str(root.val) + "->")
-        #-----------------------------------------------------------------
-        res = []
-        helper(root, "")
-        return res"""
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        
-        def helper(root,subset):
-            if root.left==None and root.right==None:
-                res.append("->".join(subset))
-                return
-            if root.left!=None:
-                subset.append(str(root.left.val))
-                helper(root.left,subset)
-                subset.pop()
+
+        def helper(root,path):
+            if root==None:
+                return ""
             
-            if root.right!=None:
-                subset.append(str(root.right.val))
-                helper(root.right,subset)
-                subset.pop()
-        #------------------------
+            if root.left==None and root.right==None:
+                res.append(path+str(root.val))
+                return
+            helper(root.left,path+str(root.val)+'->')
+            helper(root.right,path+str(root.val)+'->')
+        #------------------
         res=[]
-        helper(root,[str(root.val)])
+        helper(root,"") # subset=[]
         return res
