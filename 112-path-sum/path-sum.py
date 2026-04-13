b3c155191d8e828de95f2cffb779749c4c51f251
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-04-13 15:48:33 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (10.47%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -1,8 +1,10 @@
 
 class Solution:
-    def hasPathSum(self, root: Optional[TreeNode], target: int) -> bool:
+    #default arguments subset=[]
+    def hasPathSum(self, root: Optional[TreeNode], target: int,subset=[]) -> bool:
         if root==None:
             return False
-        if root.left==None and root.right==None and target-root.val==0:
+        if root.left==None and root.right==None and sum(subset)+root.val==target:
+            print(subset)
             return True
-        return self.hasPathSum(root.left,target-root.val) or self.hasPathSum(root.right,target-root.val)
+        return self.hasPathSum(root.left,target,subset+[root.val]) or self.hasPathSum(root.right,target,subset+[root.val])
