08c26d9f85eed1658c1d68bd1c7afee21ca55d38
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-04-15 10:29:41 +0530

    Time: 2 ms (48.61%) | Memory: 19.5 MB (30.67%) - LeetSync

--- 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.py
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -1,14 +1,14 @@
 class Solution:
     def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        #preorder = [1,2,4,5,3,6,7], postorder = [4,5,2,6,7,3]
         def maketree():
-            if postorder==[]:
-                return
-            node=TreeNode(postorder.pop())
-            if node.val!=preorder[-1]:
-                node.right=maketree()
-            if node.val!=preorder[-1]:
-                node.left=maketree()
-            preorder.pop()
-            return node
+            if postorder!=[]:
+                node=TreeNode(postorder.pop()) # node=[1]
+                if node.val!=preorder[-1]:
+                    node.right=maketree()
+                if node.val!=preorder[-1]:
+                    node.left=maketree()
+                preorder.pop()
+                return node
+
         return maketree()
-        
